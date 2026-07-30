89dd4729b24499682c5d39b4c93b201e296ce54f
zuzusnasna <jsmh991102@gmail.com>
2026-07-30 15:51:41 +0900

    변수

---
+++ Java/src/chap02/_01_variable1.java
@@ -0,0 +1,17 @@
+package chap02;
+
+public class _01_variable1 {
+    public static void main(String[] args) {
+        int level= 10;
+        System.out.println(level);
+
+        int num = 100;
+        System.out.println(num);
+
+        System.out.println(num + 4);
+        System.out.println(num - 4);
+        System.out.println(num * 4);
+        System.out.println(num / 4);
+
+    }
+}
