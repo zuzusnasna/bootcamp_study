6c059571ef013346e22e55e03b041f0de7ced1d0
zuzusnasna <jsmh991102@gmail.com>
2026-07-30 16:38:43 +0900

    변수

---
+++ Java/src/chap02/_01_Quiz.java
@@ -0,0 +1,11 @@
+package chap02;
+
+public class _01_Quiz {
+    public static void main(String[] args) {
+        int java = 88;
+        int python = 92;
+        int html = 84;
+        int average = (java + python + html)/3;
+        System.out.println(average);
+    }
+}
